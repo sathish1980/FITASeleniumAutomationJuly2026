52afcf924ebc4b2d2b582341b044c9e92cae8b72
sathish1980 <kumar.sathish189@gmail.com>
2025-06-22 12:21:42 +0530

    new selenium project

---
+++ src/main/java/Insurance/FITASeleniumAutomationJuly2026/App.java
@@ -0,0 +1,10 @@
+package Insurance.FITASeleniumAutomationJuly2026;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
